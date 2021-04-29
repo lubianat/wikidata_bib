092d4424d6030fd0c217e94e52ac0f6cdb31e6fe
lubianat <tiago.lubiana.alves@usp.br>
2021-04-29 15:03:27 -0300

    code: Start intergrating metadata.yaml

--- src/update_dashboard.py
+++ src/update_dashboard.py
@@ -15,6 +15,42 @@ import urllib.parse
 import pandas as pd
 import unicodedata
 import wbib.queries
+import yaml
+
+query_options = {
+    "map of institutions": {
+        "label": "Map of institutions",
+        "query": wbib.queries.get_query_url_for_locations,
+    },
+    "100 most recent articles": {
+        "label": "100 most recent articles",
+        "query": wbib.queries.get_query_url_for_articles,
+    },
+    "list of authors": {
+        "label": "list of authors",
+        "query": wbib.queries.get_query_url_for_authors,
+    },
+    "list of topics": {
+        "label": "list of co-studied topics",
+        "query": wbib.queries.get_topics_as_table,
+    },
+    "list of journals": {
+        "label": "list of co-studied topics",
+        "query": wbib.queries.get_query_url_for_venues,
+    },
+}
+
+
+with open("metadata.yaml") as f2:
+    config = yaml.load(f2, Loader=yaml.FullLoader)
+
+sections_to_add = []
+
+for query_names in config["sections"]:
+    for key, value in query_names.items():
+        query_options[key]["label"] = value
+        print(value)
+        sections_to_add.append(key)
 
 
 def render_dashboard(readings):
