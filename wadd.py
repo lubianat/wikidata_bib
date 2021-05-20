e67a55d2ead68f345fd438272c26c04a2fd36168
lubianat <tiago.lubiana.alves@usp.br>
2021-05-20 10:59:13 -0300

    feat: Set arguments as positional

--- wadd.py
+++ wadd.py
@@ -10,15 +10,27 @@ parser = argparse.ArgumentParser()
 # $ python3 wadd.py --url https://w.wiki/3MDs --new
 
 parser.add_argument(
-    "--url", "-u", help="A short link provided by the Wikidata Query Service", type=str
+    "url", help="A short link provided by the Wikidata Query Service", type=str
 )
+parser.add_argument(
+    "category",
+    help="The category to which the list will be appended",
+    nargs="?",
+    default=None,
+)
+
 parser.add_argument(
     "--new",
     help="Select only the articles that are not in the notes folder",
     action="store_true",
 )
+
+
 parser.add_argument(
-    "--category", "-c", help="The category to which the list will be appended"
+    "--print",
+    "-p",
+    help="Print QIDs instead of adding to the toread file",
+    action="store_true",
 )
 
 
@@ -28,6 +40,7 @@ args = parser.parse_args()
 url = args.url
 only_new = args.new
 category = args.category
+to_print = args.print
 
 session = requests.Session()  # so connections are recycled
 resp = session.head(url, allow_redirects=True)
@@ -35,6 +48,7 @@ url_sparql = resp.url.replace(
     "https://query.wikidata.org/#", "https://query.wikidata.org/sparql?query="
 )
 
+print(url_sparql)
 if "work" not in url_sparql:
     raise ValueError("SPARQL query needs to select a variable called ?work")
 
@@ -101,6 +115,7 @@ def add_to_file(qids, category, filepath="toread.md"):
         f2.writelines(to_read)
 
 
-if category != None:
+if category != None and to_print == False:
     print("====== Appending QIDs to file toread.md ====== ")
     add_to_file(df["qid"], category)
+
