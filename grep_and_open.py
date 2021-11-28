0c2a5a995360369b7ea26a1b8fbff656b021f18c
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2021-11-28 12:09:13 -0300

    Add list support to grep_and_open

--- grep_and_open.py
+++ grep_and_open.py
@@ -2,13 +2,42 @@
 
 import os
 import sys
+import inquirer
+import re
+
+questions = [
+    inquirer.List(
+        "search",
+        message="What topic are you looking for",
+        choices=[
+            "1.1. The quest for interoperable knowledge",
+            "1.2. Formal representation of knowledge",
+            "1.3. Knowledge Representation in biology",
+            "1.3.2. Biological knowledgebases",
+            "3.1. Cell-type markers in Wikidata",
+        ],
+    ),
+]
+try:
+    sys.argv[1]
+    print(sys.argv[1])
+
+    if sys.argv[1] == "ls":
+        answers = inquirer.prompt(questions)
+        search_string = answers["search"].split(" ")[0]
+        search_string = re.escape(search_string)
+
+    else:
+        search_string = text = input("Add your search string:")
 
 
-search_string = text = input("Add your search string:")
+except:
+    search_string = text = input("Add your search string:")
+    pass
 
+# Future maybe remove false positives: grep -r "3\.1" *.md | grep -v "\.3\.1"
 os.system(
     f"""
-#!/usr/bin/zsh
 cd "$(dirname "{sys.argv[0]}")"
 cd notes
 grep -r --color=always  "{search_string}"
