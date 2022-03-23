c8123d575d325b998187b6078f8f006e8506b7ad
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2022-03-23 09:37:30 -0300

    Extract function for clarity

--- src/get_qids_from_europe_pmc.py
+++ src/get_qids_from_europe_pmc.py
@@ -1,30 +1,17 @@
 import urllib.parse
 import requests
-from helper import pmid_to_wikidata_qid, add_to_file, remove_read_qids
+from helper import (
+    pmid_to_wikidata_qid,
+    add_to_file,
+    remove_read_qids,
+    get_qids_from_europe_pmc,
+)
 import re
 
 category = "Cell types"
-endpoint = "https://www.ebi.ac.uk/europepmc/webservices/rest/search"
+query = '"catalog of cell types"'
 
-params = {"query": '"catalog of cell types"', "format": "json", "pageSize": "1000"}
-response = requests.get(
-    "https://www.ebi.ac.uk/europepmc/webservices/rest/search", params
-)
-
-data = response.json()
-pmids = []
-
-
-for article in data["resultList"]["result"]:
-    try:
-        pmid = article["pmid"]
-        print(pmid)
-        pmids.append(pmid)
-    except:
-        continue
-
-print(pmids)
-main_list = pmid_to_wikidata_qid(pmids)
+main_list = get_qids_from_europe_pmc(query)
 
 # Ignore articles in toread.md
 with open("toread.md", "r") as f:
