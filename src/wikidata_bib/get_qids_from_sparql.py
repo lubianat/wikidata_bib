d524c2a9db183230472eaa82fe8338f25cf6c1b3
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2022-12-21 10:47:32 -0300

    Start coding update from yaml

---
+++ src/wikidata_bib/get_qids_from_sparql.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+import requests
+from glob import glob
+import argparse
+import pandas as pd
+import re
+from wikidata_bib.helper import add_to_file, remove_read_qids, get_qids_in_reading_list
+
+import os
+import yaml
+from pathlib import Path
+
+HERE = Path(__file__).parent.resolve()
+DATA = HERE.parent.joinpath("data").resolve()
+
+
+def get_qids_from_query(query_url):
+    """
+    Returns a list of QIDs given a SPARQL query. QIDs are returned for the projection '?work' .
+    """
+    session = requests.Session()  # so connections are recycled
+    response = session.head(query_url, allow_redirects=True)
+    url_sparql = response.url.replace(
+        "https://query.wikidata.org/#", "https://query.wikidata.org/sparql?query="
+    )
+
+    if "work" not in url_sparql:
+        raise ValueError("SPARQL query needs to select a variable called ?work")
+
+    sparql_query_response = requests.get(url_sparql, params={"format": "json"})
+    sparql_query_result = pd.json_normalize(sparql_query_response.json()["results"]["bindings"])
+    sparql_query_result["qid"] = [url.split("/")[4] for url in sparql_query_result["work.value"]]
+
+    return list(sparql_query_result["qid"])
+
+
+def update_from_yaml():
+    config = yaml.load(DATA.joinpath("config.yaml").read_text(), Loader=yaml.FullLoader)
+
+    for cat in config["queries"]:
+
+        queries = config["queries"][cat]
+        for query in queries:
+
+            full_category_name = config["lists"][cat]
+            qids = get_qids_from_query(query)
+            main_list = remove_read_qids(main_list)
+            if category is not None:
+                print("====== Appending QIDs to file toread.yaml ====== ")
+                add_to_file(main_list, full_category_name)
